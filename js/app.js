d0f4d633c34b1da3d817af940539ff9894e082e3
Forrest <forrestjtait@hotmail.com>
2015-01-15 11:48:12 -0700

    Add spaceship that can move via keyboard

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 // app.js
+function start(){
+  sjs.open();
+  var ship = new sjs.Image('img/fighter1.png', 150, 100);
+  sjs.keyDown(RIGHT_KEY, function(){ 
+     ship.pushRight();
+  });
+  sjs.keyDown(LEFT_KEY, function(){ 
+     ship.pushLeft();
+  });
+  sjs.keyDown(UP_KEY, function(){
+     ship.pushUp();
+  });
+  sjs.keyDown(DOWN_KEY, function(){
+    ship.pushDown();
+  });
+}
