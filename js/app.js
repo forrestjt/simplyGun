0b390f81a5130a1c98f000358637420700a660a6
Mark Nadal <mark@accelsor.com>
2015-01-15 12:08:05 -0700

    join game

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 // app.js
 function start(){
   sjs.open();
-  var ship = new sjs.Image('img/fighter1.png', 150, 100);
-  sjs.keyDown(RIGHT_KEY, function(){ 
+  var mark = {entity:{}}, ship;
+  var join = new sjs.Button("Join Game", function(){
+    ship = mark.entity['1'] = new sjs.Image('img/fighter1.png', 150, 100);
+  }).bottom().right();
+  sjs.keyDown(RIGHT_KEY, function(){
      ship.pushRight();
+     mark.log(ship);
   });
-  sjs.keyDown(LEFT_KEY, function(){ 
+  sjs.keyDown(LEFT_KEY, function(){
      ship.pushLeft();
+     mark.log(ship);
   });
   sjs.keyDown(UP_KEY, function(){
      ship.pushUp();
+     mark.log(ship);
   });
   sjs.keyDown(DOWN_KEY, function(){
     ship.pushDown();
+    mark.log(ship);
   });
+  mark.log = function(){ console.log.apply(console, arguments) }
 }
